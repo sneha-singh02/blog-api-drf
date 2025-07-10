0864861f06fa39c1d9a4f4d19c6dae1df312e984
Sneha Singh <snehasingh19@gmail.com>
2025-07-10 15:57:52 +0530

    Added filtering to blogpost list view

--- mysite/settings.py
+++ mysite/settings.py
@@ -13,17 +13,16 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
+
+
 SECRET_KEY = 'django-insecure-q=ti^9bqu=ish*6&ha=))!yvx332inxgfg^!xd#%pnme2g+2a^'
 
-# SECURITY WARNING: don't run with debug turned on in production!
+
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
@@ -75,16 +74,15 @@ TEMPLATES = [
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/5.2/ref/settings/#databases
+
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'blogapi_db_pcev',  # from Render
-        'USER': 'blogapi_db_pcev_user',  # from Render
+        'NAME': 'blogapi_db_pcev',  
+        'USER': 'blogapi_db_pcev_user',  
         'PASSWORD': 'SPhl19qJZiqP713FzV0uw26dMur0cqb2', 
-        'HOST': 'dpg-d1n68aumcj7s73bnef30-a',  # from Render
+        'HOST': 'dpg-d1n68aumcj7s73bnef30-a',  
         'PORT': '5432',  # standard PostgreSQL port
     }
 }
@@ -94,8 +92,7 @@ DATABASES = {
 
 
 
-# Password validation
-# https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -114,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 
 # Internationalization
-# https://docs.djangoproject.com/en/5.2/topics/i18n/
+
 
 LANGUAGE_CODE = 'en-us'
 
@@ -128,7 +125,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-import os
+
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
@@ -138,6 +135,11 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 # Default primary key field type
-# https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5  
+}
